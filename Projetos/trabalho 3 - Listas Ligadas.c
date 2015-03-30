ca73c294b09f7f84a64e1ca0a09f26bac6a2c1b5
Rafael G <gibim6@gmail.com>
2015-03-30 15:44:54 -0300

    Update trabalho 3 - Listas Ligadas.c

--- Projetos/trabalho 3 - Listas Ligadas.c
+++ Projetos/trabalho 3 - Listas Ligadas.c
@@ -1,76 +1,75 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define TRUE 1
-#define FALSE 0
+struct s_aluno{
+    int ra;
+    char nome[52];
+    float nota;
+    struct s_aluno *prox;
+};
+typedef struct s_aluno s_aluno;
 
-typedef struct{
-	int ra;
-	char nome[52];
-	float nota;
-	s_aluno *prox;
-}s_aluno;
+/*pplista: ponteiro para o ponteiro que
+aponta para o começo da lista
+no: ponteiro que aponta para o novo nó */
+void insere_nome (s_aluno *pplista, s_aluno *no){
+    pplista; //quero que apontem pro mesmo endereço
 
-void insere_nome(s_aluno *fim_lista){ //deve receber um ponteiro para o fim da lista
-		
-		printf("Digite o nome: ");  
-  	fgets(fim_lista->nome);
-  	printf("Digite o ra: "); 	
-		scanf("%i", fim_lista->RA);
- 		printf("Digite o ra: ");	
-		scanf("%f", fim_lista->nota);
+    if(pplista == NULL) //Se a lista estiver vazia
+        *pplista = *no; //a lista agora aponta para o novo nó
+    else //Senão, a tento no bagulho seguinte
+        insere_nome(&(*pplista->prox), no); //<<<<aqui pode dar merda no *prox
 }
 
-void remove_nome(s_aluno *anterior){ //deve receber um ponteiro para a struct anterior ser removida
+void remove_nome (s_aluno *pplista, int ra_removido){
+    s_aluno *p = pplista;
+    if(p != NULL){
+        if(p->ra == ra_removido){
+            pplista = p->prox;
+            free(p);
+        }
+        else remove_nome(&(*p->prox), ra_removido); //VAI DAR MERDA
+    }
 }
 
-/* recebe um ponteiro para o começo da lista
-	 retorna a posição anterior ao procurado */
-(s_aluno *) procura_por_ra(int ra, s_aluno *lista){
+void imprime_lista(s_aluno *pplista){
+    if(pplista == NULL)
+        printf("Lista vazia\n");
+    else{
+        printf("Nome: %s\n", pplista->nome);
+        printf("RA: %d\n", pplista->ra);
+        printf("Nota: %f\n", pplista->nota);
+        imprime_lista(pplista->prox); //VAI DAR MERDA
+    }
 }
-
-/* recebe um ponteiro para o começo da lista
-	 retorna a posição anterior ao procurado */
-(s_aluno *) procura_por_nome(char nome[], s_aluno *lista){
-}
-
-/* recebe a lista */
-void exibe_cadastros(s_aluno *lista){
-}
-
-void main(){
-	s_aluno *lista, *retorno;
-	int op;
-	do{
-		system("clear");
-		puts("\nDigite a opcao desejada:");
-		puts("1. Inserir cadastro de aluno");
-		puts("2. Remover cadastro de aluno");
-		puts("3. Buscar aluno por RA");
-		puts("4. Buscar aluno por nome");
-		puts("5. Exibir dados de todos os alunos cadastrados\n");
-		puts("0. Sair\n");
-
-		printf("Digite a opcao desejada");
-		scanf("%d", &op);
-		__fpurge(stdin);
-		switch(op){
-			case 1:
-			isere_nome(*);
-      break;
-			case 2;
-
-			break;
-			case 3:
-
-			break;
-			case 4:
-				retorno = procura_por_nome;
-
-			break;
-			case 5:
-				exibe_cadastros(lista);
-			break;
-		}
-	}while(	
-}
+            
+int main(void){
+    s_aluno lista;
+    lista.prox = NULL;
+    
+    s_aluno *Lista = NULL; //inicializando a lista
+    s_aluno *No = (s_aluno *)malloc(sizeof(s_aluno));
+    
+    int op;
+    
+    puts("Digite a opcao desejada:");
+    puts("1- Inserir cadastro");
+    puts("2- Remover cadastro");
+    puts("3- Buscar por RA");
+    puts("4- Buscar por Nome");
+    puts("Exibir dadps de todos alunos cadastrados");
+    
+    switch(op){
+    case 1:
+        printf("Digite o nome: ");
+        fgets(lista.nome, 52, stdin);
+        printf("Digite o RA: ");
+        scanf("%d",&lista.ra);
+        printf("Digite a nota: ");
+        scanf("%f",&lista.nota);
+        insere_aluno(Lista, &lista);
+        break;
+   }
+   
+   return 0;
+ }
