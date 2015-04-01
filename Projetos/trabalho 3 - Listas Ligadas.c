2655618e189fdb85538409df124ac11fa5026476
Rafael G <gibim6@gmail.com>
2015-04-01 14:25:12 -0300

    Update trabalho 3 - Listas Ligadas.c

--- Projetos/trabalho 3 - Listas Ligadas.c
+++ Projetos/trabalho 3 - Listas Ligadas.c
@@ -22,7 +22,7 @@ s_aluno *cria_aluno(int novo_ra, char novo_nome[], float nova_nota){
     return novo_no;
 }
 
-void insere_aluno(s_aluno **pplista, s_aluno *no){ //insere struct no inici
+void insere_aluno(s_aluno **pplista, s_aluno *no){ //insere struct no inicio
     if(*pplista != NULL) // verifico se a lista nao ta vazia
         no->prox = *pplista; //se nao tiver, ponteiro do no aponta para onde apontava o ponteiro do inicio da lista
     *pplista = no; //lista agora aponta para o endereço para onde nó aponta
@@ -30,40 +30,40 @@ void insere_aluno(s_aluno **pplista, s_aluno *no){ //insere struct no inici
 
 void exibe_alunos(s_aluno *pplista){
     s_aluno *p;
-	p = pplista;
-	if(p == NULL)
-		printf("Erro, lista vazia\n");		
+    p = pplista;
+    if(p == NULL)
+        printf("Erro, lista vazia\n");        
     else
-		for(p = pplista; p!=NULL; p = p->prox){
-	        printf("RA: %d\n", p->ra);
-	        printf("Nome: %s", p->nome);
-	        printf("Nota: %.2f\n", p->nota);
-	    }
+        for(p = pplista; p!=NULL; p = p->prox){
+            printf("RA: %d\n", p->ra);
+            printf("Nome: %s", p->nome);
+            printf("Nota: %.2f\n", p->nota);
+        }
 }
 
-			
+            
 
 //Remove funciona. Preciso estudar essa merda.
 void remove_aluno(s_aluno **pplista, int ra){
     s_aluno *aux = *pplista;
-	s_aluno *anterior = NULL;
+    s_aluno *anterior = NULL;
 
     if(aux == NULL)
         printf("Erro: lista vazia");
-	else{
-		while(aux != NULL && aux->ra != ra){
-			anterior = aux;
-			aux = aux->prox;
-		}
-		if(aux == NULL)
-			printf("Elemento nao encontrado\n");
-		else{ //se ele cair, VAI REMOVER ALGUMA COISA
-			if(anterior == NULL)
-				*pplista = aux->prox;
-			else
-				anterior->prox = aux->prox;
-			printf("Removido com sucesso!");
-		}
+    else{
+        while(aux != NULL && aux->ra != ra){
+            anterior = aux;
+            aux = aux->prox;
+        }
+        if(aux == NULL)
+            printf("Elemento nao encontrado\n");
+        else{ //se ele cair, VAI REMOVER ALGUMA COISA
+            if(anterior == NULL)
+                *pplista = aux->prox;
+            else
+                anterior->prox = aux->prox;
+            printf("Removido com sucesso!");
+        }
     }
 }
 
@@ -92,14 +92,14 @@ int main(){
                 scanf("%f", &nota);
                 insere_aluno(&Lista, cria_aluno(ra,nome,nota)); //envio começo da lista e um novo nó criado on-the-fly
                 break;
-			case 2:
-				printf("Digite o RA do registro a ser removido: ");
-				scanf("%d", &ra);				
-				remove_aluno(&Lista, ra);
-				break;
+            case 2:
+                printf("Digite o RA do registro a ser removido: ");
+                scanf("%d", &ra);                
+                remove_aluno(&Lista, ra);
+                break;
             case 5:
                 exibe_alunos(Lista);
-				break;
+                break;
         }
     }while(op != 0);
     return 0;
