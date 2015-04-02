3b619e38bc1d449a57121f04f33b6b3ad667833b
Rafael G <gibim6@gmail.com>
2015-04-01 21:22:50 -0300

    Versão final pronta pra colocar no AVA

--- Projetos/Projeto 3 - Listas Ligadas.c
+++ Projetos/Projeto 3 - Listas Ligadas.c
@@ -5,7 +5,7 @@ Rodrigo Groot 14129027
 Vinicius Nunes 14119960
 */
 
-#include <stdio_ext.h> //consigo usar __fpurge(stdin) sem warnings (warning acusado usando gcc -wall)
+#include <stdio_ext.h> //consigo usar __fpurge(stdin) sem warnings (com stdio.h: warning acusado usando gcc -wall)
 #include <stdlib.h>
 #include <string.h>
 
