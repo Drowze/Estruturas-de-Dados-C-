bf2bce84aa122622cb3c64251ebaf6aea5ec1b37
Rafael G <gibim6@gmail.com>
2015-05-11 04:17:53 -0300

    Update lib_musicas.h

--- projeto_musicas/lib_musicas.h
+++ projeto_musicas/lib_musicas.h
@@ -12,12 +12,13 @@ struct s_musica{
 };
 typedef struct s_musica s_musica;
 
+/* Funções feias e não modulares */
 s_musica *cria_musica();
-void exibe_musica(s_musica *musica);
-void copia_musica(s_musica *destino, s_musica *origem);
-void altera_musica(s_musica **lista, s_musica *no_alterado);
+int exibe_musica(s_musica *musica);
+int copia_musica(s_musica *destino, s_musica *origem);
+int altera_musica(s_musica **lista, s_musica *no_alterado);
 
-
+/* Funções bonitas e modulares */
 int adicionar_musica(s_musica **lista, s_musica *novo_no);
 int remove_musica(s_musica **lista, s_musica *no_removido);
 s_musica *busca_musica(s_musica *lista, char titulo[], char artista[]);
